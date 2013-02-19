9b844415b841021198a7d00f63d4a1daabc500c6
David Eby <eby@broadinstitute.org>
2013-02-19 20:19:56 +0000

    Updated to generate a new set of test output in order to deal with Pablo's recent suggestions re: scaling.  Also added a couple of examples based on his 'zoo' input data set as that is what he wanted to see as a validation.
    git-svn-id: https://svn.broadinstitute.org/gp2/trunk/modules/RankNormalize@29798 47fccdf2-e235-0410-87e5-63258e95d6e5

--- test/gen_test_data.R
+++ test/gen_test_data.R
@@ -1,119 +1,138 @@
 
-source("normalize.R")
+#source("normalize.R")
+source("common.R")
+source("CNMF.4.R")
 
 
 MSIG.Preprocess.Dataset(
    input.ds            = "../all_aml_test.gct",
-   output.ds           = "../test_out/defaults/valid_pset_1_test/all_aml_test.NORM.gct"
-   #normalization       = 2
+   output.ds           = "../test_out/defaults/valid_pset_1_test/all_aml_test.NORM.gct",
+   normalization       = 6
    )
 
 MSIG.Preprocess.Dataset(
    input.ds            = "../all_aml_test.res",
-   output.ds           = "../test_out/defaults/valid_pset_2_test/all_aml_test.NORM.res"
-   #normalization       = 2
+   output.ds           = "../test_out/defaults/valid_pset_2_test/all_aml_test.NORM.res",
+   normalization       = 6
    )
 
 MSIG.Preprocess.Dataset(
    input.ds            = "../all_aml_test.gct",
    output.ds           = "../test_out/optional_1/valid_pset_3_test/all_aml_test.NORM.gct",
-scale=1000
-   #normalization       = 6
+   scale=100,
+   normalization       = 6
    )
 
 MSIG.Preprocess.Dataset(
    input.ds            = "../all_aml_test.gct",
    output.ds           = "../test_out/optional_2/valid_pset_1_test/all_aml_test.NORM.gct",
-scale=10000
-   #normalization       = 6
+   scale=1,
+   normalization       = 6
    )
 
 MSIG.Preprocess.Dataset(
    input.ds            = "../all_aml_test.gct",
    output.ds           = "../test_out/optional_1/valid_pset_4_test/all_aml_test.NORM.gct",
-thres=0.00001
-   #normalization       = 2
+thres=0.00001,
+   normalization       = 6
    )
 
 MSIG.Preprocess.Dataset(
    input.ds            = "../all_aml_test.gct",
    output.ds           = "../test_out/optional_2/valid_pset_2_test/all_aml_test.NORM.gct",
-scale=10000,
-thres=0.00001
-   #normalization       = 6
+scale=1,
+thres=0.00001,
+   normalization       = 6
    )
 
 MSIG.Preprocess.Dataset(
    input.ds            = "../all_aml_test.gct",
    output.ds           = "../test_out/optional_1/valid_pset_5_test/all_aml_test.NORM.gct",
-thres=100
-   #normalization       = 2
+thres=100,
+   normalization       = 6
    )
 
 MSIG.Preprocess.Dataset(
    input.ds            = "../all_aml_test.gct",
    output.ds           = "../test_out/optional_2/valid_pset_3_test/all_aml_test.NORM.gct",
-scale=10000,
-thres=100
-   #normalization       = 6
+scale=1,
+thres=100,
+   normalization       = 6
    )
 
 MSIG.Preprocess.Dataset(
    input.ds            = "../all_aml_test.gct",
    output.ds           = "../test_out/optional_1/valid_pset_6_test/all_aml_test.NORM.gct",
-ceil=25000
-   #normalization       = 2
+ceil=25000,
+   normalization       = 6
    )
 
 MSIG.Preprocess.Dataset(
    input.ds            = "../all_aml_test.gct",
    output.ds           = "../test_out/optional_2/valid_pset_4_test/all_aml_test.NORM.gct",
-scale=10000,
-ceil=25000
-   #normalization       = 6
+scale=1,
+ceil=25000,
+   normalization       = 6
    )
 
 MSIG.Preprocess.Dataset(
    input.ds            = "../all_aml_test.gct",
    output.ds           = "../test_out/optional_1/valid_pset_7_test/all_aml_test.NORM.gct",
-ceil=1000
-   #normalization       = 2
+ceil=1000,
+   normalization       = 6
    )
 
 MSIG.Preprocess.Dataset(
    input.ds            = "../all_aml_test.gct",
    output.ds           = "../test_out/optional_2/valid_pset_5_test/all_aml_test.NORM.gct",
-scale=10000,
-ceil=1000
-   #normalization       = 6
+scale=1,
+ceil=1000,
+   normalization       = 6
    )
 
 MSIG.Preprocess.Dataset(
    input.ds            = "../all_aml_test.gct",
    output.ds           = "../test_out/optional_1/valid_pset_8_test/all_aml_test.NORM.gct",
-shift=10000
-   #normalization       = 2
+shift=10000,
+   normalization       = 6
    )
 
 MSIG.Preprocess.Dataset(
    input.ds            = "../all_aml_test.gct",
    output.ds           = "../test_out/optional_1/valid_pset_9_test/all_aml_test.NORM.gct",
-shift=10
-   #normalization       = 2
+shift=10,
+   normalization       = 6
    )
 
 MSIG.Preprocess.Dataset(
    input.ds            = "../all_aml_test.gct",
    output.ds           = "../test_out/optional_2/valid_pset_6_test/all_aml_test.NORM.gct",
-scale=10000,
-shift=1000
-   #normalization       = 6
+scale=1,
+shift=1000,
+   normalization       = 6
    )
 
 MSIG.Preprocess.Dataset(
    input.ds            = "../all_aml_test.gct",
    output.ds           = "../test_out/optional_2/valid_pset_7_test/all_aml_test.NORM.gct",
-scale=10000,
-shift=-1000
-   #normalization       = 6
+scale=1,
+shift=-1000,
+   normalization       = 6
+   )
+
+source("common.R")
+source("CNMF.4.R")
+
+
+MSIG.Preprocess.Dataset(
+   input.ds            = "../zoo.t.gct",
+   output.ds           = "../test_out/defaults/valid_pset_3_test/zoo.t.NORM.gct",
+   normalization       = 6
+   )
+
+MSIG.Preprocess.Dataset(
+   input.ds            = "../zoo.t.gct",
+   output.ds           = "../test_out/optional_2/valid_pset_8_test/zoo.t.NORM.gct",
+   scale=1,
+   normalization       = 6
    )
